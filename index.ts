5a25304ac71ebb1179cbafde8b121d52e6152e44
hoangducbinh <hoangbinh02022002@gmail.com>
2024-09-07 13:42:07 +0700

    server

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express'
+import connectToDatabase from './connection'
+
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req : Request, res: Response) => {
+  res.send('Hello World!') 
+})
+connectToDatabase()
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}/`)
+})
